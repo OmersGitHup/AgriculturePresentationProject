aad76b4a9887b278193c25a5ad822620c1e07f04
OmersGitHup <117158464+OmersGitHup@users.noreply.github.com>
2024-07-03 14:11:46 +0300

    Fixed admin panel message listing

---
+++ AgriculturePresentation/ViewComponents/NavBarMessage.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.Concrete.EntityFreamework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgriculturePresentation.ViewComponents
+{
+    public class NavBarMessage:ViewComponent
+    {
+
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = contactManager.GetListAll();
+            return View(values);
+        }
+    }
+}
